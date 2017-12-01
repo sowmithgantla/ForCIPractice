c65a9bf619c5b15bbbf4838540b7acf46092411b
sowmith <sowmithgantla@gmail.com>
2017-12-01 11:45:05 -0600

    renaming the file

---
+++ helloworld.sh
@@ -0,0 +1,2 @@
+#!/bin/bash[D[D[D[D[D[D[D[D[D[D[D[D[[[C#![C[D[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
+echo "Hello World"
